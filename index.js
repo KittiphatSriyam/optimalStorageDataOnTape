1533ab6b9398e5296bb944736061679ff5ea4ed3
unknown <skbjo032106@hotmail.com>
2019-08-15 00:09:57 +0700

    optimalStorageDataOnTape

---
+++ index.js
@@ -0,0 +1,43 @@
+const optimalStorageDataOnTape = (...tape) => {
+  console.log("tape->>", tape);
+  let que = [];
+  for (let loop in tape) {
+    que = [...que, ...tape.slice(0, Number(loop) + 1)];
+  }
+  let time = que.reduce((total, num) => total + num, 0);
+  console.log("time->>", time);
+  return time;
+};
+
+/*
+
+    [][][]   () + () + () =
+    6, 3, 8; 
+
+    Ex : 5, 10, 3
+
+    5, 10, 3;
+    5, 3, 10;
+    10, 5, 3;
+    10, 3, 5;
+    3, 5, 10; 
+*/
+
+let tape = [5, 10, 3];
+optimalStorageDataOnTape(...tape);
+console.log("------------------------");
+let tape1 = [5, 3, 10];
+optimalStorageDataOnTape(...tape1);
+console.log("------------------------");
+let tape2 = [10, 5, 3];
+optimalStorageDataOnTape(...tape2);
+console.log("------------------------");
+let tape3 = [10, 3, 5];
+optimalStorageDataOnTape(...tape3);
+console.log("------------------------");
+let tape4 = [3, 5, 10];
+optimalStorageDataOnTape(...tape4);
+console.log("------------------------");
+let tape5 = [3, 10, 5];
+optimalStorageDataOnTape(...tape5);
+console.log("------------------------");
